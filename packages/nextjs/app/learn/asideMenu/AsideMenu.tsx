b13e5153ae73998c08ad2a06184945c5c231ed7d
gabimancini <gabimancinir@gmail.com>
2024-05-16 17:51:21 -0300

    Add styles to buttons

--- packages/nextjs/app/learn/asideMenu/AsideMenu.tsx
+++ packages/nextjs/app/learn/asideMenu/AsideMenu.tsx
@@ -1,17 +1,33 @@
 import AsideMenuBtn from "./AsideMenuBtn";
-import { BsBook, BsJournal, BsLightningCharge, BsStars } from "react-icons/bs";
+import { BsBook, BsJournals, BsAward, BsStars } from "react-icons/bs";
 
 const AsideMenu = () => {
   return (
     <div className="flex flex-col">
       <AsideMenuBtn
-        className="flex items-center bg-lightYellow text-darkOrange rounded-20 p-2 border-[1px] border-darkOrange"
+        className="flex items-center bg-lightYellow text-darkOrange rounded-20 p-2 border-[1px] border-darkOrange mb-2"
         value="Ruta de aprendizaje"
         icon={BsBook}
+        fill="darkOrange"
       />
-      <AsideMenuBtn className="flex items-center text-active rounded-20 p-2" value="-" icon={BsLightningCharge} />
-      <AsideMenuBtn className="flex items-center rounded-20 p-2" value="Ver otros cursos" icon={BsJournal} />
-      <AsideMenuBtn className="flex items-center rounded-20 p-2" value="Staking" icon={BsStars} />
+      <AsideMenuBtn
+        className="flex items-center rounded-20 border-[1px] border-borderGrey p-2 mb-2"
+        value="Mis logros"
+        icon={BsAward}
+        fill="#68CC58"
+      />
+      <AsideMenuBtn
+        className="flex items-center rounded-20 border-[1px] border-borderGrey p-2 mb-2"
+        value="Ver otros cursos"
+        icon={BsJournals}
+        fill="#68CC58"
+      />
+      <AsideMenuBtn
+        className="flex items-center rounded-20 border-[1px] border-borderGrey p-2 mb-2"
+        value="Staking"
+        icon={BsStars}
+        fill="#828282"
+      />
     </div>
   );
 };
