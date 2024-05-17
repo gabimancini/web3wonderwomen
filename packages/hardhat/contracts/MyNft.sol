9e7a1f0899bbe9dd94405a67f2b65660052f4cb1
jeanette.munoz <jeanette.munoz@adalove.com.ar>
2024-05-17 11:36:58 -0300

    Add contrat NFT

---
+++ packages/hardhat/contracts/MyNft.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyNFT is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
