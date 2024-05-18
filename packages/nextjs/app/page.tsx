131ab566d8506ddd832bfa20caf16c9597893144
jeanette.munoz <jeanette.munoz@adalove.com.ar>
2024-05-18 00:43:25 -0300

    Delete imports dont use in page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import Head from "next/head";
-import Learn from "./learn/page";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
 import { Advantages } from "~~/components/Advantages";
 import { Hero } from "~~/components/Hero";
 import { Methodology } from "~~/components/Methodology";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
   return (
     <>
       <>
